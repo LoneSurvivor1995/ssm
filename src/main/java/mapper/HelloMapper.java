64d4557bb66931bc12b9e70072dee802da886e95
LoneSurvivor1995 <412018423@qq.com>
2018-09-13 15:03:03 +0800

    ssm demo

--- src/main/java/mapper/HelloMapper.java
+++ src/main/java/mapper/HelloMapper.java
@@ -1,4 +1,10 @@
 package mapper;
 
+import entity.User;
+
+import java.util.List;
+
 public interface HelloMapper {
+
+    public List<User> get();
 }
--- src/main/java/service/impl/HelloServiceImpl.java
+++ src/main/java/service/impl/HelloServiceImpl.java
@@ -1,12 +1,20 @@
 package service.impl;
 
+import mapper.HelloMapper;
 import org.springframework.stereotype.Service;
 import service.HelloService;
 
+import javax.annotation.Resource;
+
 @Service
 public class HelloServiceImpl implements HelloService {
+
+    @Resource
+    HelloMapper helloMapper;
+
     @Override
     public String get() {
-        return "HelloServiceImpl get ...";
+        System.out.println(helloMapper.get().get(0));
+        return "success";
     }
 }
